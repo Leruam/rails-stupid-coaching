1d4cd2617912682f42d4a979346332504ece31e3
antoine maurel <antoine.maurel@gmail.com>
2020-11-09 14:27:56 +0100

    added answer with algo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,14 @@ class QuestionsController < ApplicationController
 
   def answer
     # question =  params[];
+    if params[:question] == "I am going to work right now!"
+      @answer = ""
+    elsif params[:question].end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  return @answer
   end
+
 end
